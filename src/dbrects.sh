140d60a2e34a6de6a2ed7668ab3fa37e3eae5e94
howff <howff>
2023-01-15 21:53:58 +0000

    dbrects.sh, dbtags.sh - accept database directory as param

--- src/dbrects.sh
+++ src/dbrects.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-sqlite3 -separator , -cmd 'select * from DicomRects' dcmaudit.sqlite.db < /dev/null
+# Display the DicomRects table
+# Usage: database
+#  where database may be:
+#    path to database directory
+#    database file itself
+db="$1"
+if [ "$db" == "" ]; then
+    db=$SMI_ROOT/data/dicompixelanon
+fi
+if [ -d "$db" ]; then
+    db="$db"/dcmaudit.sqlite.db
+fi
+sqlite3 -separator , -cmd 'select * from DicomRects' "$db" < /dev/null
--- src/dbtags.sh
+++ src/dbtags.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-sqlite3 -separator , -cmd 'select * from DicomTags' dcmaudit.sqlite.db < /dev/null
+# Display the DicomTags table
+# Usage: database
+#  where database may be:
+#    path to database directory
+#    database file itself
+db="$1"
+if [ "$db" == "" ]; then
+    db=$SMI_ROOT/data/dicompixelanon
+fi
+if [ -d "$db" ]; then
+    db="$db"/dcmaudit.sqlite.db
+fi
+sqlite3 -separator , -cmd 'select * from DicomTags' "$db" < /dev/null
