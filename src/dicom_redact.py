dicom_redact - fixed calculation of bitmask keep to uint16 datatype to ensure negative numbers are not used at any point

src/dicom_redact.py
@@ -106,9 +106,11 @@ def remove_overlays_in_high_bits(ds):
     # bits_stored is the number of meaningful bits within those allocated.
     bits_stored = ds['BitsStored'].value if 'BitsStored' in ds else -1
     bits_allocated = ds['BitsAllocated'].value if 'BitsAllocated' in ds else -1
-    bit_mask = (~((~0) << bits_stored))
     samples = ds['SamplesPerPixel'].value if 'SamplesPerPixel' in ds else -1
     photometric = ds['PhotometricInterpretation'].value if 'PhotometricInterpretation' in ds else 'MONOCHROME2'
+
+    # Calculate bit mask to keep only the bits in use not the overlays
+    bit_mask = np.uint16( (1<<bits_stored)-1 )
 
     # This code calculates a bit mask from the actual overlays in use.
     # It is not used.
@@ -169,9 +171,11 @@ def redact_rectangles_from_high_bit_overlay(ds, overlay=0, rect_list=[]):
     bits_stored = ds['BitsStored'].value if 'BitsStored' in ds else -1
     bits_allocated = ds['BitsAllocated'].value if 'BitsAllocated' in ds else -1
     overlay_bit = overlay_bit_position(ds, overlay)
-    bit_mask = ~(1 << overlay_bit)
     samples = ds['SamplesPerPixel'].value if 'SamplesPerPixel' in ds else -1
     photometric = ds['PhotometricInterpretation'].value if 'PhotometricInterpretation' in ds else 'MONOCHROME2'
+
+    # Calculate mask to remove only the bit used by this particular overlay
+    bit_mask = np.uint16( 0xffff ^ ( 1 << overlay_bit) )
 
     # Can only handle greyscale or palette images
     # XXX would an overlay every be present in an RGB image? Doesn't make sense?
@@ -213,7 +217,9 @@ def redact_rectangles_from_image_frame(ds, frame=0, rect_list=[]):
     photometric = ds['PhotometricInterpretation'].value if 'PhotometricInterpretation' in ds else 'MONOCHROME2'
     num_frames = ds['NumberOfFrames'].value if 'NumberOfFrames' in ds else 1
     bits_stored = ds['BitsStored'].value if 'BitsStored' in ds else -1
-    bit_mask = ((~0) << bits_stored)
+
+    # Calculate mask to set pixel to black without breaking high bit overlays
+    bit_mask = np.uint16( 0xffff << bits_stored )
 
     if frame >= num_frames:
         logger.error('cannot redact frame %d, max is %d' % (frame, num_frames-1))