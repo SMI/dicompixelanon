84d944b30289994a9f35d486d3d8535a204d9ea4
howff <howff>
2023-04-12 08:41:31 +0100

    dicomrectdb - add option to query_rects to ignore rectangles which were whitelisted

--- src/library/DicomPixelAnon/dicomrectdb.py
+++ src/library/DicomPixelAnon/dicomrectdb.py
@@ -215,8 +215,8 @@ class DicomRectDB():
             rc.append(row['filename'])
         return rc
 
-    def query_rects(self, filename, frame = -1, overlay = -1):
-        """ Return a list of DicomRect objects for the given filename
+    def query_rects(self, filename, frame = -1, overlay = -1, ignore_whitelisted = False):
+        """ Return a list of DicomRectText objects for the given filename
         by reading from the database.
         Filtered to just the given frame and overlay, if specified,
         or returns all if both frame and overlay are unspecified or -1.
@@ -224,9 +224,13 @@ class DicomRectDB():
         rc = []
         for row in self.db(self.db.DicomRects.filename == filename).select():
             if ((row.frame == frame) and (row.overlay == overlay)) or (frame == -1 and overlay == -1):
-                dicomrect = DicomRect(top = row.top, bottom = row.bottom,
+                dicomrect = DicomRectText(top = row.top, bottom = row.bottom,
                     left = row.left, right = row.right,
-                    frame = row.frame, overlay = row.overlay)
+                    frame = row.frame, overlay = row.overlay,
+                    ocrengine = row.ocrengine, ocrtext=row.ocrtext,
+                    nerengine = row.nerengine, nerpii = row.nerpii)
+                if ignore_whitelisted and (nerengine == NEREnum.whitelist) and (nerpii == 0):
+                    continue
                 rc.append(dicomrect)
         return rc
 
