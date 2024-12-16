cf585c3708dff0a72f4fdf31901328fe009b001c
howff <howff>
2024-12-16 12:35:11 +0000

    dcmaudit - Move S3CredentialStore to dcmaudit_s3.py

--- src/applications/dcmaudit.py
+++ src/applications/dcmaudit.py
@@ -44,6 +44,7 @@ from DicomPixelAnon.dicomimage import DicomImage
 from DicomPixelAnon import deidrules
 from DicomPixelAnon import ultrasound
 from DicomPixelAnon.s3url import s3url_create
+from dcmaudit_s3 import S3CredentialStore
 
 
 # Configuration:
@@ -103,46 +104,6 @@ class GridEntryDialog(tkinter.simpledialog.Dialog):
 
     def apply(self):
         pass
-
-
-# =====================================================================
-class S3CredentialStore:
-    """ Store S3 credentials in home directory
-    """
-    def __init__(self):
-        self.cred_path = os.path.join(os.environ.get('HOME','.'), CREDS_FILENAME)
-        self.creds = dict()
-        self.read_creds()
-    def read_creds(self):
-        """ Returns a dict mapping nickname to a tuple (access,secret,endpoint)
-        """
-        self.creds = dict()
-        if os.path.isfile(self.cred_path):
-            with open(self.cred_path, newline='') as fd:
-                csvr = csv.DictReader(fd)
-                for row in csvr:
-                    self.creds[row['name']] = (row['access'], row['secret'], row['endpoint'])
-        return self.creds
-    def read_cred(self, nickname):
-        """ Returns tuple (access,secret) or None
-        """
-        self.read_creds()
-        (acc,sec,srv) = self.creds.get(nickname, ('',''))
-        return (acc,sec,srv)
-    def add_cred(self, nickname, access, secret, endpoint):
-        """ Appends to the CSV
-        XXX possibly re-read the file in case someone else modified it,
-        and merge with the values held in self.creds?
-        """
-        self.creds[nickname] = (access, secret, endpoint)
-        if not access or not secret:
-            del self.creds[nickname]
-        with open(self.cred_path, 'w', newline='') as fd:
-            csvw = csv.DictWriter(fd, fieldnames=['name','access','secret','endpoint'], lineterminator='\n')
-            csvw.writeheader()
-            for nickname in self.creds:
-                (acc,sec,srv) = self.creds[nickname]
-                csvw.writerow( { 'name': nickname, 'access': acc, 'secret': sec, 'endpoint': srv } )
 
 
 # =====================================================================
@@ -211,12 +172,6 @@ class S3CredentialsDialog:
 
 
 # =====================================================================
-
-class S3LoadPrefs:
-    def __init__(self):
-        self.output_dir = None
-        self.csv_file_dir = None
-        pass
 
 class S3LoadDialog:
     def __init__(self, parent, s3loadprefs = None):
