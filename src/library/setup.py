a79e37d0c6da9e9a2e8ac221a51878fdb423befe
howff <3064316+howff@users.noreply.github.com>
2024-12-18 13:58:05 +0000

    setup.py - strip off +suffix from version numbers in requirements.txt

--- src/library/setup.py
+++ src/library/setup.py
@@ -21,6 +21,8 @@ def translate_req(req):
         if op in req:
             req, version = req.split(op)
             version = op + version
+            if '+' in version
+                version = version.split('+')[0]
     if version:
         req += '(%s)' % version
     return req
