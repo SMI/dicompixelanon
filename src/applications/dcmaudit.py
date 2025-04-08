2642ed80c45b62c006110a4dee957ec4ef258cd2
howff <howff>
2025-04-08 17:54:19 +0100

    dcmaudit - add option to download single files from a bucket, instead of displaying the file as an image.

--- src/applications/dcmaudit.py
+++ src/applications/dcmaudit.py
@@ -220,7 +220,127 @@ class S3CredentialsDialog:
 
 # =====================================================================
 
+class S3DownloadDialog:
+    """ Pop up a window giving the ability to download a file from S3
+    by selecting credentials, populating a listbox with the files in
+    the bucket, and downloading the selected file to a given directory.
+    """
+    def __init__(self, parent):
+        self.bucket = None
+        self.access = self.secret = None
+        self.output_dir = None
+        # Read the stored credentials
+        cred_store = S3CredentialStore()
+        cred_list = cred_store.read_creds()
+        cred_names = list(cred_list.keys())
+        if not cred_names:
+            tkinter.messagebox.showerror(title="No credentials",
+                message='Please use the credential manager to save some credentials')
+            return
+        # Construct GUI
+        top = self.top = tkinter.Toplevel(parent)
+        top.geometry(f'+{max(0,parent.winfo_rootx()-20)}+{parent.winfo_rooty()}')
+        tkinter.Label(top, text='Saved credentials:').grid(row=0, column=0)
+        self.bucket_dropdown = tkinter.StringVar()
+        def pick(xx):
+            self.bucket = self.bucket_dropdown.get()
+            (self.access, self.secret, self.endpoint) = cred_store.read_cred(self.bucket)
+        self.bucketMenu = tkinter.OptionMenu(top, self.bucket_dropdown, *cred_names, command = pick)
+        ToolTip(self.bucketMenu, msg="Select the name of the bucket where the files are stored", delay=TTD)
+        self.bucketMenu.grid(row=0, column=1)
+        # List of files with scrollbar
+        self.filelist = []
+        self.filelistvar = tkinter.StringVar(value=self.filelist)
+        self.myFileList = tkinter.Listbox(top, listvariable=self.filelistvar, height=10)
+        ToolTip(self.myFileList, msg="Pick a file to download")
+        self.myFileList.grid(row=1, column=0, sticky=(tkinter.N,tkinter.W,tkinter.E,tkinter.S))
+        scroller = tkinter.Scrollbar(top, orient=tkinter.VERTICAL, command=self.myFileList.yview)
+        self.myFileList.configure(yscrollcommand=scroller.set)
+        scroller.grid(row=1, column=1, sticky=(tkinter.N,tkinter.S))
+        # Output directory
+        tkinter.Label(top, text='Output directory (use ~ for your home directory):').grid(row=2, column=0)
+        self.outputEntry = tkinter.Entry(top)
+        ToolTip(self.outputEntry, msg="Enter an output directory, use ~ for your home directory", delay=TTD)
+        self.outputEntry.grid(row=2, column=1)
+        # List and Download buttons
+        self.myListButton = tkinter.Button(top, text='List', command=self.list)
+        self.myListButton.grid(row=3, column=0)
+        ToolTip(self.myListButton, msg="List the files in the chosen bucket.", delay=TTD)
+        self.myDownloadButton = tkinter.Button(top, text='Download', command=self.download)
+        self.myDownloadButton.grid(row=3, column=1)
+        ToolTip(self.myDownloadButton, msg="Download the selected file into the Output directory.", delay=TTD)
+        top.grid_columnconfigure(0, weight=1)
+        top.grid_rowconfigure(0, weight=1)
+
+    def list(self):
+        if not self.access or not self.secret:
+            tkinter.messagebox.showerror(title="Error", message='Please select some credentials')
+            return
+        # Connect to S3 service
+        logging.debug('Logging into S3 at %s with %s:%s' % (self.endpoint, self.access, self.secret))
+        try:
+            s3 = boto3.resource('s3',
+                endpoint_url=self.endpoint,
+                aws_access_key_id=self.access, aws_secret_access_key=self.secret)
+        except:
+            tkinter.messagebox.showerror(title="Error", message="Cannot connect to the S3 server, check the endpoint URL and the credentials in the credential manager")
+            return
+        # Select the bucket given by the credential name
+        s3bucket = s3.Bucket(name=self.bucket)
+        try:
+            for obj in s3bucket.objects.filter(Prefix = ''):
+                self.filelist.append(obj.key)
+                self.filelistvar.set(self.filelist)
+        except:
+            tkinter.messagebox.showerror(title="Error", message="Cannot connect to the S3 server, check the endpoint URL and the credentials in the credential manager")
+            return
+        return
+
+    def download(self):
+        if not self.access or not self.secret:
+            tkinter.messagebox.showerror(title="Error", message='Please select some credentials')
+            return
+        self.output_dir = os.path.expanduser(os.path.expandvars(self.outputEntry.get()))
+        if not self.output_dir:
+            tkinter.messagebox.showerror(title="Error", message='Please enter an output directory, for example use ~ for your home directory, or ~/s3 for the s3 directory inside your home directory.')
+            return
+        selected_indices = self.myFileList.curselection()
+        if selected_indices:
+            selected_index = selected_indices[0]
+            selected_item = self.myFileList.get(selected_index)
+            print("Selected item:", selected_item)
+        else:
+            tkinter.messagebox.showerror(title="Error", message='Please select a filename.')
+            return
+        # Connect to S3 service
+        logging.debug('Logging into S3 at %s with %s:%s' % (self.endpoint, self.access, self.secret))
+        try:
+            s3 = boto3.resource('s3',
+                endpoint_url=self.endpoint,
+                aws_access_key_id=self.access, aws_secret_access_key=self.secret)
+        except:
+            tkinter.messagebox.showerror(title="Error", message="Cannot connect to the S3 server, check the endpoint URL and the credentials in the credential manager")
+            return
+        # Select the bucket given by the credential name
+        s3bucket = s3.Bucket(name=self.bucket)
+        try:
+            for obj in s3bucket.objects.filter(Prefix = selected_item):
+                print('Download %s to %s' % (obj.key, self.output_dir))
+                os.makedirs(os.path.dirname(self.output_dir), exist_ok=True)
+                s3bucket.download_file(obj.key, os.path.join(self.output_dir, os.path.basename(obj.key)))
+                tkinter.messagebox.showinfo(title="Error", message="Downloaded "+os.path.basename(obj.key))
+        except Exception as e:
+            raise e
+            tkinter.messagebox.showerror(title="Error", message="Cannot download %s from S3 server, check the endpoint URL and the credentials in the credential manager, and check the output directory is writeable.")
+            return
+        return
+
+# =====================================================================
+
 class S3LoadDialog:
+    """ Display a window giving options to download or view a set
+    of images held in a S3 bucket.
+    """
     def __init__(self, parent, s3loadprefs = None):
         # Initialise class variables
         self.bucket = None
@@ -582,7 +702,8 @@ class App:
         self.openmenu.add_command(label='Open directory recursive', command=lambda: self.open_directory_event(None, True))
         self.openmenu.add_separator()
         self.openmenu.add_command(label='Manage S3 credentials', accelerator='Ctrl+3', command=lambda: self.manage_s3_event(None))
-        self.openmenu.add_command(label='Open files from S3', accelerator='Ctrl+S', command=lambda: self.open_s3_event(None))
+        self.openmenu.add_command(label='Open DICOM from S3', accelerator='Ctrl+S', command=lambda: self.open_s3_event(None))
+        self.openmenu.add_command(label='(Download reports from S3)', command=lambda: self.report_s3_event(None))
         self.openmenu.add_separator()
         self.openmenu.add_command(label='Choose database directory', command=lambda: self.open_db_directory_event(None))
         self.openmenu.add_command(label='Export database of rectangles as CSV', command=lambda: self.save_db_csv_event(None, rects=True, tags=False))
@@ -759,6 +880,15 @@ class App:
         self.set_image_list(FileList(s3load.path_list))
         self.load_next_file()
 
+    def report_s3_event(self, event):
+        """ Display dialogue box to pick a file to download from S3.
+        """
+        root = self.tk_app
+        s3load = S3DownloadDialog(root)
+        # If dialogue box never got constructed due to error then
+        # it won't have a 'top' so doesn't need to be destroyed.
+        if hasattr(s3load, 'top'):
+            root.wait_window(s3load.top)
 
     def save_db_csv_event(self, event, rects = False, tags = False):
         """ Pop up a file dialog box asking for a CSV filename.
